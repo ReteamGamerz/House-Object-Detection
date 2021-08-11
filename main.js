f129c4ccc77242b87691f23a5d2644d656faf09a
ReteamGamerz <76695753+ReteamGamerz@users.noreply.github.com>
2021-08-11 14:47:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function redirect_bed() {
+    window.location = "bedroom.html";
+}
+
+function redirect_living() {
+    window.location = "living.html";
+}
+
+function redirect_kitchen() {
+    window.location = "kitchen.html";
+}
+
+function redirect_dine() {
+    window.location = "dining.html";
+}
